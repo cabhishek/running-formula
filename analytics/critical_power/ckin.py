fe5133e1771eb4a38f9f185315b4e1baa4a5fa66
Abhishek Kapatkar <cabhishek@gmail.com>
2014-09-20 15:32:21 -0400

    Formulas for calculating Caero and Ckin.

---
+++ analytics/critical_power/ckin.py
@@ -0,0 +1,17 @@
+import math
+
+def ckin(distance, time):
+    """
+        Ckin = Enery required to change kinetic energy
+
+        ckin = 0.5 * n^-1 * d * t^-2
+
+        Where
+            n = 0.25
+            d = distance in meters
+            t = total time in secs
+    """
+    n = 0.25
+
+    return 0.5 * math.pow(n, -1) * distance * math.pow(time, -2)
+
